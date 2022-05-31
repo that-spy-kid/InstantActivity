035eb6f09e35a6bd79489c93bf0b9441245ac7ce
Vinay Kumar <vinay173.viit@gmail.com>
2022-05-31 12:08:54 +0530

    ✅

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const bodyParser = require("body-parser");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let activityData;
+
+app.get("/", (req,res)=>{
+    res.render("activity", {activity: activityData});
+    res.redirect("/");
+});
+
+app.post("/", (req,res)=>{
+     
+    const querry = req.body.typeName;
+    const url = "http://www.boredapi.com/api/activity?type=" + querry;
+    
+    
+    http.get(url, (response)=>{
+        response.on("data", (data)=>{
+            const activityList = JSON.parse(data);
+            activityData = activityList.activity;
+           res.redirect("/"); 
+        });
+    });
+
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log("server is running on port 3000");
+});
